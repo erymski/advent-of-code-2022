781c81a8b6442a8b0555acf1fd77d6342964dd3e
Eugene Rymski <erymski@gmail.com>
2023-03-12 22:35:33 +0400

    Day 02, first half

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,39 +1,73 @@
-use std::fs::File;
+use std::fs;
 use std::env;
-use std::io::{BufReader, BufRead, Seek};
 
-fn first_half(file: &File) -> std::io::Result<()> {
+const A: u8 = 'A' as u8;
+const X: u8 = 'X' as u8;
 
-    let reader = BufReader::new(file);
+const ROCK: u8 = 1;
+const PAPER: u8 = 2;
+const SCISSORS: u8 = 3;
 
-    for line in reader.lines() {
+fn game_points(opponent_move: u8, my_move: u8) -> u32 {
 
+    if opponent_move == my_move {
+        return 3;
+    } else {
+
+        return match (opponent_move, my_move) {
+            (ROCK, PAPER) => 6,
+            (PAPER, SCISSORS) => 6,
+            (SCISSORS, ROCK) => 6,
+            (_, _) => 0
+        };
     }
-    Ok(())
+}
+
+fn first_half(lines: std::str::Lines) {
+
+    let mut points: u32 = 0;
+
+    for line in lines {
+        let bytes = line.as_bytes();
+        let opponent_move: u8 = bytes[0] - A + 1;
+        let my_move: u8 = bytes[2] - X + 1;
+
+        let round_pts = game_points(opponent_move, my_move);
+        points += round_pts;
+        points += my_move as u32;
+
+        println!("{} vs {} => {} + {}", opponent_move, my_move, round_pts, my_move);
+    }
+
+    println!("1) Total points {}", points);
 }
 
 
-fn second_half(file: &File) -> std::io::Result<()> {
+// fn second_half(file: &File) -> std::io::Result<()> {
 
-    let reader = BufReader::new(file);
+//     let reader = BufReader::new(file);
 
-    for line in reader.lines() {
+//     for line in reader.lines() {
 
-    }
-    Ok(())
-}
+//     }
+//     Ok(())
+// }
 
 fn main() -> std::io::Result<()> {
 
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 { panic!("Not enough command line parameters"); }
+
     let filename: &String = &args[1];
     println!("\nIncoming path: {}", filename);
 
-    let mut file = File::open(filename)?;
-    first_half(&file)?;
+    // let it = "A Y\nB X\nC Z".lines();
+    let file_content = fs::read_to_string(filename)?;
+    let it = file_content.lines();
+    first_half(it);
 
-    file.rewind()?;
-    second_half(&file)?;
+    // file.rewind()?;
+    // second_half(&file)?;
 
     Ok(())
 }
