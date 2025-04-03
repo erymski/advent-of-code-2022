d48619f8c1c655d9520bf63385f3dd300265cd9d
Eugene Rymski <erymski@gmail.com>
2025-04-03 23:04:07 +0200

    Part 2 of day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -22,6 +22,18 @@ impl Range {
     fn either_contains(&self, other: &Range) -> bool {
         return self.contains(other) || other.contains(self) 
     }
+
+    fn overlap(&self, other: &Range) -> bool {
+        return self.any_inside(other) || other.any_inside(self); 
+    }
+
+    fn inside(&self, pt :u8) -> bool {
+        return (self.from <= pt) && (pt <= self.to);
+    }
+
+    fn any_inside(&self, other: &Range) -> bool {
+        return self.inside(other.from) || self.inside(other.to); 
+    }
 }
 
 /// convert string like "83-83,57-89" to pair of Ranges
@@ -35,12 +47,19 @@ fn to_pair(line: &str) -> (Range, Range) {
 fn main() -> std::io::Result<()> {
     
     let content = utils::load_data()?;
-    let pairs = content.lines().map(to_pair);
+    let pairs: Vec<(Range, Range)> = content.lines().map(to_pair).collect(); // TODO: how to convert to array?
+
     let count1 = pairs
+                        .iter()
                         .filter(|(a,b)|a.either_contains(b))
                         .count();
     println!("1) The count is {}", count1);
 
+    let count2 = pairs
+                        .iter()
+                        .filter(|(a,b)|a.overlap(b))
+                        .count();
+    println!("2) The count is {}", count2);
 
     Ok(())
 }
@@ -55,6 +74,25 @@ mod tests {
         assert!(Range { from: 0, to: 3}.contains(&Range { from: 1, to: 3 }));
         assert!(Range { from: 0, to: 3}.contains(&Range { from: 0, to: 2 }));
         assert!(! Range { from: 0, to: 3}.contains(&Range { from: 1, to: 5 }));
+    }
+
+    #[test]
+    fn inside_check() {
+        assert!(Range { from: 0, to: 3}.inside(1));
+        assert!(Range { from: 0, to: 3}.inside(0));
+        assert!(Range { from: 0, to: 3}.inside(3));
+        assert!(! Range { from: 1, to: 3}.inside(0));
+        assert!(! Range { from: 0, to: 3}.inside(4));
+    }
+
+    #[test]
+    fn overlapping() {
+        assert!(Range { from: 0, to: 3}.overlap(&Range { from: 1, to: 2 }));
+        assert!(Range { from: 0, to: 3}.overlap(&Range { from: 1, to: 3 }));
+        assert!(Range { from: 0, to: 3}.overlap(&Range { from: 0, to: 2 }));
+        assert!(Range { from: 0, to: 3}.overlap(&Range { from: 1, to: 5 }));
+        assert!(Range { from: 0, to: 3}.overlap(&Range { from: 3, to: 5 }));
+        assert!(! Range { from: 0, to: 3}.overlap(&Range { from: 4, to: 5 }));
     }
 
     #[test]
